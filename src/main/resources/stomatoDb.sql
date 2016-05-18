65c89bcd22be941ffcd87505625494525f8eeacb
Sorin Vasiliu <soringabrielvasiliu@gmail.com>
2016-05-18 22:51:16 +0300

    administrator table added

--- src/main/resources/stomatoDb.sql
+++ src/main/resources/stomatoDb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2016 at 05:17 PM
--- Server version: 5.7.10-log
--- PHP Version: 5.5.12
+-- Generation Time: May 18, 2016 at 07:49 PM
+-- Server version: 5.7.9
+-- PHP Version: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,43 +17,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `questions`
+-- Table structure for table `administrators`
 --
 
-CREATE TABLE IF NOT EXISTS `questions` (
+DROP TABLE IF EXISTS `administrators`;
+CREATE TABLE IF NOT EXISTS `administrators` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `question` varchar(5000) NOT NULL,
-  `answer` varchar(500) DEFAULT NULL,
-  `used` tinyint(1) NOT NULL DEFAULT '1',
+  `user` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
-
---
--- Dumping data for table `questions`
---
-
-INSERT INTO `questions` (`id`, `question`, `answer`, `used`) VALUES
-(1, 'intrebare1', 'answer updated1', 1),
-(3, 'intrebare2', 'answer updated2', 0),
-(4, 'intrebare3', 'answer updated3', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) NOT NULL,
-  `surname` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `surname`) VALUES
-(1, 'aa', 'bar'),
-(2, 'bb', 'nis');
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
