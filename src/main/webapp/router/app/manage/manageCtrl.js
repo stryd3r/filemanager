2fc3e725cc54608f35e596b8d20f5b2376cb9122
Habu And <habuc4@gmail.com>
2016-09-03 14:22:19 +0300

    css stailing :D

--- src/main/webapp/router/app/manage/manageCtrl.js
+++ src/main/webapp/router/app/manage/manageCtrl.js
@@ -1,6 +1,9 @@
 angular.module('mainApp').controller('manageCtrl',
 		[ '$scope', '$state', 'mainService', function($scope, $state, srv) {
 			srv.getPacients().then(function(res) {
+				angular.forEach(res.data, function(elem) {
+					elem.editMode = false;
+				});
 				$scope.pacientsList = res.data;
 			});
 		} ]);
