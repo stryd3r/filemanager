6711928cbb1f8fdcf76961a4acaa1fc4809886ce
Habu And <habuc4@gmail.com>
2016-09-17 12:49:35 +0300

    test with quite real calls and not json

--- src/main/webapp/router/app/testRest/testRestCtrl.js
+++ src/main/webapp/router/app/testRest/testRestCtrl.js
@@ -1,8 +1,13 @@
 angular.module('mainApp').controller('testRestCtrl',
 		[ '$scope', '$http', function($scope, $http) {
 			$scope.getPacienti = function() {
-				$http.get('shared/mock/json/pacienti.json').then(function(res) {
+				$http.get('https://httpbin.org/get').then(function(res) {
 					$scope.status1 = res.status;
+				});
+			}
+			$scope.getPacientiPost = function() {
+				$http.post('https://httpbin.org/post').then(function(res) {
+					$scope.status2 = res.status;
 				});
 			}
 		} ]);
