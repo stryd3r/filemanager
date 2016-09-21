95f46fbd6b7d17b6089b41481f9b61952534e0df
stryd3r <stryd3r_ady@yahoo.com>
2016-09-21 14:20:22 +0300

    jUnit added

---
+++ src/test/java/jUnits/DoctoriTest.java
@@ -0,0 +1,28 @@
+package jUnits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import com.filemanager.backend.service.interfaces.PacientiService;
+import com.filemanager.config.AppConfig;
+import com.filemanager.config.AppInitializer;
+import com.filemanager.config.DataSourceConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//WebConfig is not loaded because jUnit is not working with @EnableWebMvc
+@ContextConfiguration(classes = { AppInitializer.class, AppConfig.class, DataSourceConfig.class }, loader = AnnotationConfigContextLoader.class)
+public class DoctoriTest {
+
+	@Autowired
+	private PacientiService pacientiService;
+
+	@Test
+	public void firstTest() {
+		pacientiService.getPacienti(false, false);
+		System.out.println("ok");
+	}
+}
