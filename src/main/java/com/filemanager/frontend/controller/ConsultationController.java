929f5f21a63466b2b60eea2bb1764a04f6808a47
stryd3r <stryd3r_ady@yahoo.com>
2016-10-10 11:20:42 +0300

    Method changed to Delete on removeConsultations

--- src/main/java/com/filemanager/frontend/controller/ConsultationController.java
+++ src/main/java/com/filemanager/frontend/controller/ConsultationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filemanager.backend.service.interfaces.ConsultationService;
@@ -22,8 +23,8 @@ public class ConsultationController {
 		return result;
 	}
 
-	@RequestMapping(value = "/deleteConsultations", produces = "application/json")
-	public boolean deleteConsultations(List<Integer> consultationsId) {
+	@RequestMapping(value = "/removeConsultations", produces = "application/json", method=RequestMethod.DELETE)
+	public boolean removeConsultations(List<Integer> consultationsId) {
 		boolean result = service.removeConsultations(consultationsId);
 
 		return result;
